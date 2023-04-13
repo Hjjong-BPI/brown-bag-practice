8b21a911003b55af37307e5ba815f9190793ad4e
Hans <hjjong.bpi@gmail.com>
2023-04-13 12:46:14 +0800

    add age calculator

--- greetings.py
+++ greetings.py
@@ -1,4 +1,13 @@
+def calculate_age(birthyear):
+    return 2023 - birthyear
+
 print("What is your name?")
 name = input()
 
-print(f"Hello {name}")
+print("What year were you born?")
+birthyear = input()
+birthyear = int(birthyear)
+
+age = calculate_age(birthyear)
+
+print(f"Hello {name}, you are {age} years old!")
